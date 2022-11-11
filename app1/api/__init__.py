e0d637779d9f790db3ce477528a0a049fed0fc7b
Vinay <vinay@Vinays-MacBook-Air.local>
2022-11-11 14:20:07 +0530

    updates

--- app1/api/__init__.py
+++ app1/api/__init__.py
@@ -4,5 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return { "id": "1", "message": "Hello world" }
-
+    return { "id": "1", "message": "Hello world and Vinay" }
--- app2/api/__init__.py
+++ app2/api/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
+import requests
+import sys
 
 app = Flask(__name__)
 
+def reverse():
+    print ("hitting URL app1-service:5000", file=sys.stdout)
+    x = requests.get('http://app1-service:5000')
+    print (x, file=sys.stdout)
+    str = x.json()['message']
+    revstr = ""
+    for i in str:
+        revstr = i + revstr
+    return revstr
+
 @app.route('/')
 def hello_world():
-    return { "id": "1", "message": "Hello world" }
-
+    reverseword = reverse()
+    return { "id": "1", "message": reverseword }
