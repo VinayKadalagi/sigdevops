7e19dae6d83a89d7d98e1ed325dd995d345aee1b
Vinay <vinay@Vinays-MacBook-Air.local>
2022-11-12 21:40:23 +0530

    updates

--- app1/api/__init__.py
+++ app1/api/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
+import json
+import os
 app = Flask(__name__)
+
+def read_Json_Doc():
+    script_dir = os.path.dirname(__file__) 
+    json_file = open(os.path.join(script_dir, 'data.json'))
+    data = json.load(json_file)
+    return data
 
 @app.route('/')
 def hello_world():
-    return { "id": "1", "message": "Hello world and Vinay" }
+    return read_Json_Doc()
