be884ae95ae096861ec0f3ef6c8b22cf832a3e01
Vinay <vinay@Vinays-MacBook-Air.local>
2022-11-12 21:49:00 +0530

    updates

--- app1/api/__init__.py
+++ app1/api/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 import json
 import os
+
 app = Flask(__name__)
 
+
 def read_Json_Doc():
-    script_dir = os.path.dirname(__file__) 
+    script_dir = os.path.dirname(__file__)
     json_file = open(os.path.join(script_dir, 'data.json'))
     data = json.load(json_file)
     return data
+
 
 @app.route('/')
 def hello_world():
--- app2/api/__init__.py
+++ app2/api/__init__.py
@@ -1,20 +1,19 @@
 from flask import Flask
 import requests
-import sys
 
 app = Flask(__name__)
 
+
 def reverse():
-    print ("hitting URL app1-service:5000", file=sys.stdout)
     x = requests.get('http://app1-service:5000')
-    print (x, file=sys.stdout)
     str = x.json()['message']
     revstr = ""
     for i in str:
         revstr = i + revstr
     return revstr
 
+
 @app.route('/')
 def hello_world():
     reverseword = reverse()
-    return { "id": "1", "message": reverseword }
+    return {"id": "1", "message": reverseword}
